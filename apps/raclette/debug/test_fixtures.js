8ff9433be8386a9e5cc1668e346f5cdd510485c5
Richard Klancer <rpk@pobox.com>
2010-06-23 17:48:47 -0400

    Updated setupFixtures() to create a new FixturesDataSource
    This avoids the problem with previous tests modifying the contents of SC.Record.fixtures.

--- apps/raclette/debug/test_fixtures.js
+++ apps/raclette/debug/test_fixtures.js
@@ -1,9 +1,11 @@
 var setupFixtures = function () {
+  
     Raclette.Activity.FIXTURES = [{
         guid: 1,
         title: 'Wheels of Cheese',
         questions: [1,2]
     }];
+    
     Raclette.Question.FIXTURES = [{
         guid: 1,
         prompt: "What color is the sky?",
@@ -14,8 +16,8 @@ var setupFixtures = function () {
         prompt: "What color is dirt?",
         activity: 1
     }];
-    // debugger;
-    Raclette.store.find(Raclette.Question);
-    Raclette.store.find(Raclette.Activity);
-    // Raclette.set("store",SC.Store.create().from(SC.Record.fixtures));
+
+    // Create a new data source behind Raclette.store, 
+    // thereby forcing the data source to read the FIXTURES properties anew.
+    Raclette.set("store",SC.Store.create().from(SC.FixturesDataSource.create()));
 };
