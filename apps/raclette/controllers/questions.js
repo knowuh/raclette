7aab6913206c26cbe5907a9270af9ab62864cbd1
Noah Paessel <knowuh@gmail.com>
2010-06-23 17:16:42 -0400

    new controller test.

---
+++ apps/raclette/controllers/questions.js
@@ -0,0 +1,17 @@
+// ==========================================================================
+// Project:   Raclette.question
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Raclette */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+Raclette.questionsController = SC.ArrayController.create(
+/** @scope Raclette.question.prototype */ {
+  contentBinding: 'Raclette.activityController.questions'
+
+}) ;
---
+++ apps/raclette/tests/controllers/questions.js
@@ -0,0 +1,39 @@
+// ==========================================================================
+// Project:   Raclette.question Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Raclette module test ok equals same stop start */
+
+module("Raclette.question", { setup: setupFixtures });
+
+// TODO: Replace with real unit test for Raclette.question
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
+// assume 2 questions in the fixtures
+test("Verify questions controller length is updated", function() {
+  var firstActivity;
+  
+  SC.run(function(){
+    firstActivity = Raclette.store.find(Raclette.Activity,1);
+    Raclette.activityController.set('content',firstActivity);
+  });
+  
+  // verify initial conditions:
+  equals(Raclette.questionsController.get("length"), 2, "expected 2 questions (check fixtures)");
+  
+  SC.run(function(){
+    var newQuestion = Raclette.store.createRecord(Raclette.Question,{
+      prompt: "new question"
+    });
+    newQuestion.set("activity",firstActivity);
+  });
+  
+  equals(firstActivity.getPath("questions.length"), 3, "expected 3 questions after add in model");
+  equals(Raclette.questionsController.get("length"), 3, "expected 3 questions after add in controller");
+  
+  
+});
