0c83a1d94ab747582762e2b3dafe3c4174639e70
Noah Paessel <knowuh@gmail.com>
2010-06-24 16:19:04 -0400

    Display the question list view.

--- sproutcore/apps/raclette/resources/main_page.js
+++ sproutcore/apps/raclette/resources/main_page.js
@@ -11,14 +11,23 @@ Raclette.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
-    
+    childViews: 'labelView questionsView'.w(),
     labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
-      textAlign: SC.ALIGN_CENTER,
+      layout: { top: 10, left: 10, width: 200, height: 40 },
+      textAlign: SC.ALIGN_LEFT,
       tagName: "h1", 
       valueBinding: 'Raclette.activityController.title'
+    }),
+    questionsView: SC.ListView.design({
+      layout: { top: 50, width: 120, height: 80, left: 10 },
+      contentBinding: 'Raclette.questionsController.arrangedObjects',
+      selectionBinding: 'Raclette.questionsController.selection',
+      contentValueKey: "prompt",
+      rowHeight: 21,
+      canEditContent: YES,
+      canDeleteContent: YES
     })
+    
   })
 
 });
