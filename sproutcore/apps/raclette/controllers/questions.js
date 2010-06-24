0b33fa59d661039ec83ffaff5a8baa86caf1b971
Noah Paessel <knowuh@gmail.com>
2010-06-24 16:42:39 -0400

    Added button to view to add new question.

--- sproutcore/apps/raclette/controllers/questions.js
+++ sproutcore/apps/raclette/controllers/questions.js
@@ -12,6 +12,28 @@
 */
 Raclette.questionsController = SC.ArrayController.create(
 /** @scope Raclette.question.prototype */ {
-  contentBinding: 'Raclette.activityController.questions'
+  contentBinding: 'Raclette.activityController.questions',
+  addQuestion: function() {
+      var question;
+      debugger;
+      // create new question and add it to the list
+      question = Raclette.store.createRecord(Raclette.Question, { 
+        prompt: "<your question>"
+      });
+      
+      question.set('activity',Raclette.activityController.get('content'));
+      
+      // select new task in UI
+      this.selectObject(question); 
 
+      // activate inline editor once UI can repaint
+      this.invokeLater(function() {
+        var contentIndex = this.indexOf(question);
+        var list = Raclette.mainPage.getPath('mainPane.questionsView');
+        var listItem = list.itemViewForContentIndex(contentIndex);
+        listItem.beginEditing();
+      });
+
+      return YES ;
+  }
 }) ;
--- sproutcore/apps/raclette/resources/main_page.js
+++ sproutcore/apps/raclette/resources/main_page.js
@@ -11,23 +11,44 @@ Raclette.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView questionsView'.w(),
+    childViews: 'labelView questionsView addQuestionButton'.w(),
     labelView: SC.LabelView.design({
-      layout: { top: 10, left: 10, width: 200, height: 40 },
+      layout: {
+        top: 10,
+        left: 10,
+        width: 200,
+        height: 40
+      },
       textAlign: SC.ALIGN_LEFT,
-      tagName: "h1", 
+      tagName: "h1",
       valueBinding: 'Raclette.activityController.title'
     }),
     questionsView: SC.ListView.design({
-      layout: { top: 50, width: 120, height: 80, left: 10 },
+      layout: {
+        top: 50,
+        width: 120,
+        height: 80,
+        left: 10
+      },
       contentBinding: 'Raclette.questionsController.arrangedObjects',
       selectionBinding: 'Raclette.questionsController.selection',
       contentValueKey: "prompt",
       rowHeight: 21,
       canEditContent: YES,
       canDeleteContent: YES
+    }),
+    addQuestionButton: SC.ButtonView.design({
+      layout: {
+        top: 50,
+        left: 140,
+        height: 80,
+        width: 100 
+      },
+      title: "Add Question",
+      target: "Raclette.questionsController",
+      action: "addQuestion"
     })
-    
+
   })
 
 });
