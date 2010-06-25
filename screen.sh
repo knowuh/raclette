f4cae624a4bcccca48e105994e4a10c8e9ae5f41
Noah Paessel <knowuh@gmail.com>
2010-06-25 13:39:34 -0400

    added some doc references to screen.sh

--- screen.sh
+++ screen.sh
@@ -1,9 +1,14 @@
 
-# \n \r and \013 and \015 dont seem to work
-# for crarriage return wuen stuffing commands.
+# Screen documentation used to create this:
+# http://blog.lathi.net/articles/2008/09/13/scripting-screen
+# http://www.debian-administration.org/articles/560
+# http://www.gnu.org/software/screen/manual/screen.html
+
+# \n, \r, \015 &etc. dont send <cr> to screen "stuff" cmd.
 RETURN="
 "
 RAILS_PORT="3001"
+SC_PORT="4020"
 
 # create detached screen session:
 screen -d -m -c ./.screenrc -S raclette
@@ -19,8 +24,8 @@ screen -X -S raclette -p 2 stuff "cd sproutcore; sc-server $RETURN"
 sleep 1
 
 # open browsers
-open "http://localhost:3001/" &
-open "http://localhost:4020/" &
+open "http://localhost:${RAILS_PORT}/" &
+open "http://localhost:${SC_PORT}/" &
 
 # open screen, with selection window
 screen -x raclette -p =
