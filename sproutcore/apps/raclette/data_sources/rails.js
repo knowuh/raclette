ed0399cfad956bd7e4b21348386293931944c0f7
Noah Paessel <knowuh@gmail.com>
2010-06-25 15:45:04 -0400

    Create new datasource for rails app:
     master > sc-gen data-source Raclette.RailsDataSource
     ~ Created directory at data_sources
     ~ Created file at data_sources/rails.js

---
+++ sproutcore/apps/raclette/data_sources/rails.js
@@ -0,0 +1,64 @@
+// ==========================================================================
+// Project:   Raclette.RailsDataSource
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Raclette */
+
+/** @class
+
+  (Document Your Data Source Here)
+
+  @extends SC.DataSource
+*/
+Raclette.RailsDataSource = SC.DataSource.extend(
+/** @scope Raclette.RailsDataSource.prototype */ {
+
+  // ..........................................................
+  // QUERY SUPPORT
+  // 
+
+  fetch: function(store, query) {
+
+    // TODO: Add handlers to fetch data for specific queries.  
+    // call store.dataSourceDidFetchQuery(query) when done.
+
+    return NO ; // return YES if you handled the query
+  },
+
+  // ..........................................................
+  // RECORD SUPPORT
+  // 
+  
+  retrieveRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to retrieve an individual record's contents
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  createRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit new records to the data source.
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  updateRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit modified record to the data source
+    // call store.dataSourceDidComplete(storeKey) when done.
+
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  destroyRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to destroy records on the data source.
+    // call store.dataSourceDidDestroy(storeKey) when done
+    
+    return NO ; // return YES if you handled the storeKey
+  }
+  
+}) ;
