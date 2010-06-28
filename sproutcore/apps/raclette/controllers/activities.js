46b49e35f16321e5ae01a507f45bb978fc9a14e4
Richard Klancer <rpk@pobox.com>
2010-06-28 00:09:41 -0400

    Fixed up activitiesController so that it refreshes its selection property when its content changes.
    This is necessary because if its selection is still valid after its content changes, then controllers like activityController that are bound to its selection never refresh themselves. This causes test failures, at least, when activityController fails to refresh its 'questions' property, which ends up containing questions not found in the new datastore instantiated in the next test module.

--- sproutcore/apps/raclette/controllers/activities.js
+++ sproutcore/apps/raclette/controllers/activities.js
@@ -13,23 +13,15 @@
 Raclette.activitiesController = SC.ArrayController.create(
 /** @scope Raclette.activitiesController.prototype */ {
 
-  contentDidChange: function () {
-    console.group('Raclette.activitiesController.contentDidChange');
+  allowsEmptySelection: NO,
+  allowsMultipleSelection: NO,
 
-    var content = this.get('content');
-    if (content) {
-      console.log('content.length: ', content.get('length'));
-      console.log('content: ', content);
-      if (content.get('length') > 0) {
-        console.log('length > 0, setting selection to ', content.objectAt(0));
-        this.selectObject(content.objectAt(0));
-      }
-    }
-    else {
-      console.log('content is null');
-    }
-    
-    console.groupEnd();    
-  }.observes('[]')    // '[]' is the Sproutcore key meaning 'observe the contents, not merely the identity, of this array'
+  // forces the activityController to refresh its 'questions' ManyArray when the content object changes, even if
+  // the selection left over from the old content object is valid with the new content object.
+  _contentDidChange: function () {
+    console.group('Raclette.activitiesController._contentDidChange');
+    this.selectObject(this.objectAt(0));
+    console.groupEnd();
+  }.observes('content')
 
 }) ;
