ff8ac1b1123946bb83ead63d1facd102cdf55337
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-06-30 14:43:30 -0400

    start_server uses rvm 1.9.1@raclette and more urls

--- start_server.rb
+++ start_server.rb
@@ -1,27 +1,39 @@
 #!/usr/bin/ruby
 
 require 'rubygems'
-require 'appscript'
-include Appscript
+begin
+  require 'appscript'
+  include Appscript
+rescue LoadError
+  raise <<-HEREDOC
+
+
+Please install the RubyGem: rb-appscript: "gem install rb-appscript"
+more info on rb-appscript: http://appscript.sourceforge.net/rb-appscript/"
+
+  HEREDOC
+end
 
 this_dir = File.expand_path(File.dirname(__FILE__))
 require "#{this_dir}/scripts/tab_windows.rb"
 
-
 tab_commands = [
-  [this_dir, 'git status'],
-  [this_dir,'cd rails; script/server -p 3001'], 
-  [this_dir, 'cd sproutcore; sc-server']
+  [this_dir, 'rvm use 1.9.1@raclette; git status'],
+  [this_dir,'rvm use 1.9.1@raclette; cd rails; rake db:seed; script/server -p 3001'], 
+  [this_dir, 'rvm use 1.9.1@raclette; cd sproutcore; sc-server']
 ]
 
 TabWindows.make('raclette', tab_commands)
 
-puts "sleeping 5s before opening browser windows ..."
-sleep(5)
+puts "sleeping 10s to let servers start up before opening browser windows ..."
+sleep(10)
 
 browser_urls = %w{
   http://0.0.0.0:4020/raclette
-  http://0.0.0.0:4020/static/raclette/en/current/tests.html
+  http://0.0.0.0:4020/raclette/en/current/tests.html
+  http://0.0.0.0:4020/raclette/en/current/tests/data_sources/rails_create.html
+  http://localhost:3001/rails/activities
+  http://localhost:3001/rails/questions
 }
 
 puts "opening Safari tabs"
