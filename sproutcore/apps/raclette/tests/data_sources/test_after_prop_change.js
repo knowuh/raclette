e2a133bc57f652591bef5842adc71f4fa0d8b807
Richard Klancer <rpk@pobox.com>
2010-06-30 19:18:42 -0400

    Improved tests for new testAfterPropertyChange

--- sproutcore/apps/raclette/tests/data_sources/test_after_prop_change.js
+++ sproutcore/apps/raclette/tests/data_sources/test_after_prop_change.js
@@ -2,109 +2,121 @@
 
 var obj;
 
-module("stausQueue", { 
-  setup: function() {
+module("testAfterPropertyChange", { 
+  setup: function () {
     obj = SC.Object.create({status: 0}); 
   }
 });
 
-test("testAfterPropertyChange calls callback", function(){
-  expect(1);
 
-  setTimeout(function(){
+test("testAfterPropertyChange calls callback", function () {
+  expect(1);    // expect 1 assertion, i.e., fail the test unless the testAfterPropertyChange really does call ok()
+
+  setTimeout(function () {
     obj.set('status', 1);
   }, 100);
 
-  testAfterPropertyChange(obj, 'status', function(){
+  testAfterPropertyChange(obj, 'status', function () {
     ok(true, 'callback was called');    
   });
 });
 
-test("testAfterPropertyChange fails when callback throws exception", function(){
+
+test("testAfterPropertyChange indicates the error when the callback throws an exception", function () {
   expect(1);
-  var oldOk = ok;
-  var okCalledWithFalse = false;
-  ok = function(condition){
-    if(!condition){
-      okCalledWithFalse = true;
-    }
+  var oldError = CoreTest.plan.error;
+  var errorWasCalled = false;
+  
+  CoreTest.plan.error = function () {
+    errorWasCalled = true;
   };
   
-  var wasCalled = false;
-  testAfterPropertyChange(obj, 'status', function(){
-      var bad = null;
-      bad.get('something');
-    });
+  testAfterPropertyChange(obj, 'status', function () {
+    null.get("this won't work!");
+  });
   
-  setTimeout(function(){
-    try {
-      obj.set('status', 1);
-    } catch (e) {
-      // testAfterPropertyChange throws exceptions all the way up so our exception will end up here      
-    }
-    // reset oldOk
-    ok = oldOk;
-    equals(true, okCalledWithFalse, "The ok method was called with a false condition after an exception was thrown");
+  setTimeout(function () {
+    obj.set('status', 1);
+    CoreTest.plan.error = oldError;
+    ok(errorWasCalled, "The error in the testAfterPropertyChange callback should result in a call CoreTest.plan.error()");
   }, 100);
 });
 
-test("testAfterPropertyChange handles nesting", function(){
+
+test("testAfterPropertyChange handles nesting", function () {
   expect(4);
   
-  setTimeout(function(){
+  setTimeout(function () {
     ok(true, 'first timeout was called');
     obj.set('status', 1);
-  }, 100);  
+  }, 100);
   
-  testAfterPropertyChange(obj, 'status', function(){
-    ok(true, 'callback was called first time');
-    setTimeout(function(){
+  testAfterPropertyChange(obj, 'status', function () {
+    ok(true, 'callback was called after first timeout');
+
+    setTimeout(function () {
       ok(true, 'second timeout was called');
       obj.set('status', 2);
     }, 100);
-    testAfterPropertyChange(obj, 'status', function(){
-      ok(true, 'callback was called second time');
+
+    testAfterPropertyChange(obj, 'status', function () {
+      ok(true, 'callback was called after second timeout');
     });
   });  
 });
 
-test("testAfterPropertyChange handles exceptions in first level", function(){
-  expect(6);
+
+test("testAfterPropertyChange handles exceptions without failing to call nested testAfterPropertyChange", function () {
+  expect(7);
   
-  var oldOk = ok;
-  var okCalledWithFalse = false;
-  ok = function(condition, message){
-    if(!condition){
-      okCalledWithFalse = true;
-    } else {
-      oldOk(condition, message);
-    }
+  var oldError, errorWasCalled;
+  
+  var getNumObservers = function () {
+    return obj._kvo_observers_status ? obj._kvo_observers_status.getMembers().length : 0;
   };
-  
-  setTimeout(function(){
-    ok(true, 'first timeout was called');
-    try {
-      obj.set('status', 1);
-    } catch (e) {
-      // our observer should throw an exception
-      // and log an error
-    }
-    ok = oldOk;
-    equals(true, okCalledWithFalse, "The ok method was called with a false condition after an exception was thrown");    
-  }, 100);  
-  
-  testAfterPropertyChange(obj, 'status', function(){
-    ok(true, 'callback was called first time');
-    setTimeout(function(){
-      ok(true, 'second timeout was called');
-      equals(1, obj._kvo_observers_status.getMembers().length, 'observer is still added to status');
-      obj.set('status', 2);
-    }, 100);
-    testAfterPropertyChange(obj, 'status', function(){
-      console.log('callback was called second time');
-      ok(true, 'callback was called second time');
-    });
-    throw 'fake exception';
-  });  
-});
+  var nObservers = getNumObservers();
 
+  setTimeout(function () {
+    ok(true, 'outer timeout was called');       // 1
+    obj.set('status', 1);
+    
+    CoreTest.plan.error = oldError;
+        
+    ok(errorWasCalled,                          // 2
+      "The fake exception in the callback of the outer testAfterPropertyChange should result in a call to " +
+      "CoreTest.plan.error()");
+  }, 100);
+  
+  testAfterPropertyChange(obj, 'status', function () {
+    ok(true, 'outer testAfterPropertyChange callback was called');          // 3
+    
+    // the nested testAfterPropertyChange follows:
+    setTimeout(function () {
+      ok(true, 'inner timeout was called');     // 4
+      obj.set('status', 2);
+      
+      equals(getNumObservers(), nObservers,     // 5
+        "after the inner testAfterPropertyChange callback, 'obj' should have no net change in the number of " +
+        "observers (even though the outer callback threw an exception)");
+    }, 100);
+
+    testAfterPropertyChange(obj, 'status', function () {
+      ok(true,                                  // 6
+        'inner testAfterPropertyChange callback was called despite exception in outer testAfterPropertyChange callback');
+    });
+
+    // once the nested callbacks are defined, throw an exception to see how testAfterPropertyChange handles it
+    throw 'fake exception for testing purposes';
+  });
+  
+  // remember that the following executes before the callbacks above:
+  equals(getNumObservers(), nObservers + 1,     // 7
+    "'obj' should have one more observer immediately after the outer testAfterPropertyChange() is called to set " +
+    "the callback");
+
+  oldError = CoreTest.plan.error;
+  errorWasCalled = false;
+  CoreTest.plan.error = function () {     // you really want to rewrite this LAST
+    errorWasCalled = true;
+  };
+});
