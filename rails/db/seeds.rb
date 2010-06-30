6a2baf41a73298ce26c62194f97f265a44d62c0a
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-06-30 14:39:43 -0400

    rake db:seed now erases and resets database
    The rake task db:seed runs the Ruby script: db/seeds.rb
    
    This script deletes all existing Activity and Question models
    and creates 2 Activities each of which have 2 Questions.
    
    Some of the SproutCore data source tests expect the first
    Activity to have at least one Question. Using these commands
    to start the Rails backend will assure this is true:
    
      cd rails; rake db:seed; script/server -p 3001

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -5,3 +5,18 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+Activity.delete_all
+Question.delete_all
+
+activities = Activity.create([
+  { :title => 'Rounds of Cheese' }, 
+  { :title => 'Squares of Cheese' }
+])
+
+Question.create([
+  { :prompt => 'What color is the sky?',        :activity => activities[0] }, 
+  { :prompt => 'Why is the ocean blue?',        :activity => activities[0] },
+  { :prompt => 'What is the color of money?',   :activity => activities[1] }, 
+  { :prompt => 'What is the meaning of life?',  :activity => activities[1] },
+])
